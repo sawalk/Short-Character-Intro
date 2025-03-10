64ae495ea6325fe388460e937ab9b2b7381a4b6e
sawalk <ham.step2@gmail.com>
2025-03-10 16:33:44 +0900

    major update

--- main.lua
+++ main.lua
@@ -6,7 +6,10 @@ local json = require("json")
 
 mod.config = {
     toggleKey = Keyboard.KEY_HOME,
-    language = 1
+    language = 1,
+    posY = 90,
+    scale = 0,
+    blackout = true
 }
 
 mod.language = {
@@ -65,6 +68,61 @@ if ModConfigMenu then
         end,
         Info = "Set the key to call the intro animation."
     });
+    ModConfigMenu.AddSetting("Short Character Intro", nil,
+    {
+        Type = ModConfigMenu.OptionType.NUMBER,
+        Attribute = "Y Position",
+        Minimum = 1,
+        Maximum = 800,
+        ModifyBy = 5,
+        CurrentSetting = function()
+            return mod.config.posY
+        end,
+        Display = function() return
+            "Y Position: ".. mod.config.posY
+        end,
+        OnChange = function(newOption)
+            mod.config.posY = newOption;
+        end,
+        Info = "Set the Y position of the paper. (Default 90)"
+    });
+    ModConfigMenu.AddSetting("Short Character Intro", nil,
+    {
+        Type = ModConfigMenu.OptionType.NUMBER,
+        Attribute = "Scale",
+        Minimum = 0,
+        Maximum = 1.5,
+        ModifyBy = 0.05,
+        CurrentSetting = function()
+            return mod.config.scale
+        end,
+        Display = function() return
+            "Scale: ".. mod.config.scale
+        end,
+        OnChange = function(newOption)
+            mod.config.scale = newOption;
+        end,
+        Info = "Set the size of the paper. (0 is the auto setting)"
+    });
+    ModConfigMenu.AddSetting("Short Character Intro", nil,
+    {
+        Type = ModConfigMenu.OptionType.BOOLEAN,
+        Attribute = "Blackout",
+        CurrentSetting = function()
+            return mod.config.blackout
+        end,
+        Display = function()
+            if mod.config.blackout then
+                return "Blackout: True"
+            else
+                return "Blackout: False"
+            end
+        end,
+        OnChange = function(newOption)
+            mod.config.blackout = newOption;
+        end,
+        Info = "Sets whether to make the background black when animating."
+    });
 end
 
 mod:AddPriorityCallback(
@@ -78,6 +136,10 @@ mod:AddPriorityCallback(
         local jsonString = mod:LoadData()
         mod.config = json.decode(jsonString)
         mod.config.toggleKey = mod.config.toggleKey or Keyboard.KEY_HOME
+        mod.config.language = mod.config.language or 1
+        mod.config.posY = mod.config.posY or 90
+        mod.config.scale = mod.config.scale or 0
+        mod.config.blackout = mod.config.blackout or true
     end
 )
 
@@ -94,7 +156,8 @@ mod:AddPriorityCallback(
 mod.currentAnim = nil      -- 현재 재생중인 애니메이션 이름
 mod.isAnimating = false    -- 애니메이션이 재생 중인지
 mod.showIn = true          -- 토글 상태: true면 다음은 인 애니메이션, false면 아웃 애니메이션
-mod.lastLanguage = nil
+mod.lastLanguage = nil     -- 마지막으로 설정된 언어
+mod.lastBlackout = nil     -- 마지막으로 설정된 블랙아웃 설정
 
 local function GetDescriptionLanguage()
     if mod.config.language == 1 then
@@ -105,6 +168,7 @@ local function GetDescriptionLanguage()
         return "kr"
     else
         error("[Short Character Intro]\nInvalid language configuration")
+        return "en"
     end
 end
 
@@ -120,12 +184,29 @@ local function LoadSprite()
 end
 mod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, LoadSprite)
 
+local function UpdateBackground()
+    if mod.config.blackout ~= mod.lastBlackout then
+        if mod.config.blackout then
+            sprite:ReplaceSpritesheet(1, "gfx/ui/bgblack.png")
+        else
+            sprite:ReplaceSpritesheet(1, "bgalpha.png")
+        end
+        sprite:LoadGraphics()
+        mod.lastBlackout = mod.config.blackout
+    end
+end
+
+mod:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, function()
+    mod.showIn = true
+end)
+
 mod:AddCallback(ModCallbacks.MC_POST_RENDER, function()
     local player = Isaac.GetPlayer(0)
 
     local currentLanguage = GetDescriptionLanguage()
     if mod.lastLanguage ~= currentLanguage or not sprite:IsLoaded() then
         LoadSprite()
+        mod.showIn = true
     end
 
     if mod.currentAnim then
@@ -135,22 +216,27 @@ mod:AddCallback(ModCallbacks.MC_POST_RENDER, function()
             mod.isAnimating = false
         end
 
-        local scale = 0.75
-        if Options.MaxScale == 1 or Options.MaxScale == 2 then
-            scale = 0.5
-        elseif Options.MaxScale == 3 then
-            scale = 0.666666
+        local finalScale = mod.config.scale
+        if mod.config.scale == 0 then    -- 0은 자동
+            if Isaac.GetScreenPointScale() == 1 or Isaac.GetScreenPointScale() == 2 then
+                finalScale = 0.5
+            elseif Isaac.GetScreenPointScale() == 3 then
+                finalScale = 2 / 3
+            else
+                finalScale = 0.75
+            end
         end
         
-        sprite.Scale = Vector(scale, scale)
+        sprite.Scale = Vector(finalScale, finalScale)
         sprite.Color = Color(1, 1, 1, 1, 0, 0, 0)
-        
+        UpdateBackground()
+
         local room = Game():GetRoom()
         local pos = room:WorldToScreenPosition(Vector(0,0)) - room:GetRenderScrollOffset() - Game().ScreenShakeOffset
         local rx = pos.X + 60 * 26 / 40
         local ry = pos.Y + 162.5 * (26 / 40)
         local screenSize = Vector(rx*2 + 13*26, ry*2 + 7*26)
-        sprite:Render(Vector(screenSize.X / 1.98, screenSize.Y / 3.333333), Vector(0,0), Vector(0,0))
+        sprite:Render(Vector(screenSize.X / 1.98, mod.config.posY), Vector(0,0), Vector(0,0))
     end
 
     if Input.IsButtonTriggered(mod.config.toggleKey, 0) then
